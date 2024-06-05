4113c7e05468907f7a3e3be1f6b4f9ed1705f33b
fdeevy <findeevy@gmail.com>
2024-06-05 11:55:35 +0100

    Updated Water

--- Scenes/Waves.gd
+++ Scenes/Waves.gd
@@ -14,20 +14,6 @@ func _ready():
 	waterMap.resize(MAP_LENGTH)
 	waterMap.fill(0)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(Input. is_action_just_pressed("ui_select") and MAP_LENGTH*MAP_HEIGHT >= waterCounter+waveSize):
-		for i in waveSize:
-			var waterRng=rng.randi_range(0, MAP_LENGTH-1)
-			while(MAP_HEIGHT<=waterMap[waterRng]):
-				waterRng=rng.randi_range(0, MAP_LENGTH-1)
-			var waterBlock = Sprite2D.new()
-			add_child(waterBlock)
-			waterBlock.texture = load("res://Textures/water.png")
-			waterBlock.z_index = 4
-			var waterX=waterRng*TILE_SIZE-32
-			var waterY=waterMap[waterRng]*TILE_SIZE-32
-			waterBlock.position = Vector2(waterX,waterY)
-			waterCounter+=1
-			waterMap[waterRng]=waterMap[waterRng]+1
+	pass
--- Scenes/game_board.gd
+++ Scenes/game_board.gd
@@ -6,6 +6,14 @@ signal render
 var init_array = []
 var tile_array = []
 
+var board_length = 0
+var board_height = 0
+
+var water_counter = 0
+var wave_size = 3
+var water_array = []
+var rng = RandomNumberGenerator.new()
+
 func load_array(file_name):
 	var f = FileAccess.open("res://" + file_name, FileAccess.READ)
 	while f.get_position() < f.get_length():
@@ -31,14 +39,25 @@ func load_array(file_name):
 					td.append(1)
 		init_array.append(id)
 		tile_array.append(td)
+	board_height=init_array[0].size()
+	board_length=init_array.size()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	load_array("map.dat")
+	water_array.resize(board_length)
+	water_array.fill(0)
 	render.emit()
 	render_background.emit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(Input. is_action_just_pressed("ui_select") and water_counter<board_length*board_height):
+		for i in wave_size:
+			var waterRng=rng.randi_range(0, board_length-1)
+			while(board_height<water_array[waterRng]):
+				waterRng=rng.randi_range(0, board_length-1)
+			water_counter+=1
+			water_array[waterRng]=water_array[waterRng]+1
+			print(str(water_array))
