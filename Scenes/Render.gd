b1a59f26311d6d8d7696d8b6555f9d16cb13f43e
anotherLostKitten <eggismagicchickensquared@gmail.com>
2024-06-05 06:23:47 -0400

    multi-level rendering

--- Scenes/Render.gd
+++ Scenes/Render.gd
@@ -1,37 +1,53 @@
 extends TileMap
 
+var offset = -1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	match name:
+		"TileMap":
+			offset = 0
+		"TileMap2":
+			offset = 1
+		"TileMap3":
+			offset = 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_game_board_render():
-	clear_layer(1)
+	var lr = 1 if offset == 0 else 0
+	print("my off:", offset)
+	clear_layer(lr)
 	var r = 0
 	for rd in GameBoard.tile_array:
 		var c = 0
 		for cd in rd:
-			if cd & 1 > 0:
-				set_cell(1, Vector2i(c, r), 0, Vector2i(0, 3))
-			elif cd & 2 > 0:
-				set_cell(1, Vector2i(c, r), 0, Vector2i(0, 2))
-			elif cd & 4 > 0:
-				set_cell(1, Vector2i(c, r), 0, Vector2i(0, 1))
+			if cd & 4 > 0 and offset == 0:
+				set_cell(lr, Vector2i(c, r), 0, Vector2i(0, 1))
+			elif cd & 2 > 0 and offset == 0:
+				set_cell(lr, Vector2i(c, r), 0, Vector2i(0, 2))
+			elif cd & 1 > 0 and offset == 0:
+				set_cell(lr, Vector2i(c, r), 0, Vector2i(0, 3))
+			elif cd & 6 == 6 and offset == 1:
+				set_cell(lr, Vector2i(c, r), 0, Vector2i(0, 2))
+			elif (cd + 2) & 5 == 5 and offset == 1:
+				set_cell(lr, Vector2i(c, r), 0, Vector2i(0, 3))
+			elif cd == 7 and offset == 2:
+				set_cell(lr, Vector2i(c, r), 0, Vector2i(0, 3))
 			c+=1
 		r+=1
-	notify_runtime_tile_data_update(1)
+	notify_runtime_tile_data_update(lr)
 	
 func _on_game_board_render_background():
-	clear_layer(0)
-	var r = 0
-	for rd in GameBoard.init_array:
-		var c = 0
-		for cd in rd:
-			set_cell(0, Vector2i(c, r), 0, Vector2i(0, cd + 4))
-			c+=1
-		r+=1
-	notify_runtime_tile_data_update(0)
+	if offset == 0:
+		clear_layer(0)
+		var r = 0
+		for rd in GameBoard.init_array:
+			var c = 0
+			for cd in rd:
+				set_cell(0, Vector2i(c, r), 0, Vector2i(0, cd + 4))
+				c+=1
+			r+=1
+		notify_runtime_tile_data_update(0)
