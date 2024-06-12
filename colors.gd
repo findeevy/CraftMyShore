6f56fbf55549dbaaa4071ecb9c85d275d7914af2
fdeevy <findeevy@gmail.com>
2024-06-12 11:04:27 +0100

    Updated map_editor

--- colors.gd
+++ colors.gd
@@ -3,5 +3,6 @@ extends Node
 var CITY = Color(0.91764, 0.196078, 0.23529)
 var HILL = Color(1, 0.63529, 0.078431)
 var PLANT = Color(0.11764, 0.43529, 0.31372)
+var WATER = Color(0, 0.80392, 0.97647)
 var NONE = Color(1, 1, 1)
 var TILE_SIZE = 64;
